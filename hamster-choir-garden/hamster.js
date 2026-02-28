c36490968b889faa7e4b0461fe2cf0bc1c8c69a0
AbigailEckstein <abigail.e275@gmail.com>
2026-02-28 17:43:06 -0500

    stub out hamster file

---
+++ hamster-choir-garden/hamster.js
@@ -0,0 +1,39 @@
+
+class Hamster{
+    constructor(id, name){
+        this.id = id;
+        this.name = name;
+        level = 0;
+        stage = 0;
+        multiplier = 1;
+    }
+
+    getLevel(){}
+
+    getExp(){}
+
+    water(){}
+
+    pet(){}
+
+    gainExp(){
+        //if reach level up threshold, levelUp(){}
+    }
+
+    levelUp(){
+        //if certain level reached, stageUp
+    }
+
+    stageUp(){
+        //milestone tracker
+    }
+
+
+
+
+
+
+
+
+
+}
