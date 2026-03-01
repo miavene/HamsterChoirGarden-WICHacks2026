fc3b5ad64b65402bbb67d961372a5bedd5480cbd
AbigailEckstein <abigail.e275@gmail.com>
2026-02-28 23:04:36 -0500

    factory fix

--- hamster-choir-garden/renderer/models/hamsterFactory.js
+++ hamster-choir-garden/renderer/models/hamsterFactory.js
@@ -1,8 +1,8 @@
-import {HAMSTER_TYPES} from "./data";
+import {HAMSTER_TYPES} from "../data/hamsterData";
 
 class hamsterFactory{
 
-    createHampster(hamsterData){
+    createHamster(hamsterData){
         var id = hamsterData.id;
         var name = hamsterData.name;
         var baseRevenue = hamsterData.baseRevenue;
