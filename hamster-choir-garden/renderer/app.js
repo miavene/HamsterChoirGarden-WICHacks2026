e03349d8d25c896a19cf3540b9b2c94913343b5a
shyyarg <grayshinea@gmail.com>
2026-03-01 01:53:49 -0500

    garden-1.css updated

--- hamster-choir-garden/renderer/app.js
+++ hamster-choir-garden/renderer/app.js
@@ -1,16 +1,25 @@
-//get new garden button
-const newGardenBtn = document.getElementById("new-garden");
+import User from "../renderer/models/user.js";
+import Garden from "../renderer/models/garden.js";
+
+const newGardenBtn = document.querySelector("#new-garden button");
 
 newGardenBtn.addEventListener("click", () => {
-    //change window
-    window.location.href = "../renderer/pages/garden-1/index.html";
+    // Give starting coins if first time
+    if (User.getCoinCount() === 0) {
+        User.addToCoins(75);
+    }
+
+    // Create new empty garden
+    const garden = new Garden(10); // max 10 hamsters
+
+    // Add to user's gardens
+    User.addNewGarden(garden);
+
+    // Redirect to garden page, passing the index of this garden
+    const gardenIndex = Array.from(User.gardens).length - 1;
+    window.location.href = `../pages/garden-1/index.html?garden=${gardenIndex}`;
 });
 
-//get gardens button
-const gardensBtn = document.getElementById("gardens-btn");
-
-gardensBtn.addEventListener("click", () => {
-    //change windows
-    window.location.href = "pages/gardens/index.html";
-});
-
+// get garden index from query parameter
+const params = new URLSearchParams(window.location.search);
+const gardenIndex = parseInt()
