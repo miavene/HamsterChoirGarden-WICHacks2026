bd626eef8871e657ec1f4fb1a7efaa129ca911a2
Mia Venesky <venesky.mia@gmail.com>
2026-02-28 19:48:30 -0500

    Updated directory and file structure. Updated main.js to support new structure

--- hamster-choir-garden/main.js
+++ hamster-choir-garden/main.js
@@ -7,7 +7,7 @@ const createWindow = () => {
     height: 600
   })
 
-  win.loadFile('index.html')
+  win.loadFile('renderer/index.html')
 }
 app.whenReady().then(() => {
   createWindow()
---
+++ hamster-choir-garden/renderer/data/hamsterData.js
@@ -0,0 +1,8 @@
+export const HAMSTER_TYPES = {
+    // Placeholder hamster  just to start getting structure out-- Can change completely later
+  1: {
+    // name: "Bleeblorble",
+    // multiplier: 1.1,
+    // etc can be added here as needed
+  }
+};
