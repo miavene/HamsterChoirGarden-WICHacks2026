b969aaf42213f79d508043cbeea0f8942e97c7b8
AbigailEckstein <abigail.e275@gmail.com>
2026-03-01 04:21:17 -0500

    filled out some hamsters

--- hamster-choir-garden/renderer/data/hamsterData.js
+++ hamster-choir-garden/renderer/data/hamsterData.js
@@ -3,7 +3,7 @@ export const HAMSTER_TYPES = {
   1: {
     name: "Bleeblorble",
     id: 0,
-    baseRevenue: 10,
+    baseRevenue: 3,
     song: "../data/songs/A.mp3"
     // multiplier: 1.1,
     // etc can be added here as needed
@@ -11,19 +11,32 @@ export const HAMSTER_TYPES = {
   2: {
     name: "GleebyGlomp",
     id: 1,
-    baseRevenue: 12,
+    baseRevenue: 5,
     song: "../data/songs/B.mp3"
   },
   3: {
+    name: "Glockenspiel",
+    id: 2,
+    baseRevenue: 7,
+    song: "../data/songs/C.mp3"
 
   },
   4: {
-
+    name: "Cheese",
+    id: 3,
+    baseRevenue: 9,
+    song: "../data/songs/D.mp3"
   },
   5: {
-
+    name: "Doctor Wheel",
+    id: 4,
+    baseRevenue: 10,
+    song: "../data/songs/E.mp3"
   },
   6: {
-
+    name: "Tim",
+    id: 5,
+    baseRevenue: 15,
+    song: "../data/songs/F.mp3"
   },
 };
